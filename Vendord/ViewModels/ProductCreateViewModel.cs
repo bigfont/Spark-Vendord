1f53cf51a0a87b3777bb43509a582cd0527638f7
Shaun Luttin <shaunluttin@gmail.com>
2013-08-24 17:20:03 -0700

    Add ProductCreateViewModel to its own file.

---
+++ Vendord/ViewModels/ProductCreateViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Vendord.DAL;
+using System.Linq;
+
+namespace Vendord.ViewModels
+{
+    public class ProductCreateViewModel : IValidatableObject
+    {
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Product Name", Prompt = "Type the product name here.")]
+        public string Name { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            VendordContext db = new VendordContext();            
+            if (db.Products.Any(p => p.Name == this.Name))
+            {
+                yield return new ValidationResult("The Product Name must be unique.", new string[] { "Name" });
+            }
+        }
+    }
+}
